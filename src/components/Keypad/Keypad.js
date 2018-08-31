af81603bfac10ad5379b604fbae0ce233f2f37a5
JacobTovsen <jacobtovsen@gmail.com>
2018-08-31 16:38:14 -0500

    keypad layout

---
+++ src/components/Keypad/Keypad.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>/</button>
+        <br></br>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>*</button>
+        <br></br>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>+</button>
+        <br></br>
+        <button>0</button>
+        <button> . </button>
+        <button>=</button>
+        <button>-</button>
+      </div>
+    );
+  }
+}
+
+export default App;
