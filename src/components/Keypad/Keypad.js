f14c60e504d05f006535fb99ae8cc94548bafb0f
JacobTovsen <jacobtovsen@gmail.com>
2018-09-02 12:48:03 -0500

    operator functions

--- src/components/Keypad/Keypad.js
+++ src/components/Keypad/Keypad.js
@@ -1,29 +1,57 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      firstValue: 0,
+      mathOperator: '',
+      secondValue: 0,
+    }
+  }
+
+
+  firstValue = (value) =>  {
+    this.setState({
+      firstValue: value
+    })
+    console.log('in firstValue', value);
+  }
+
+  setOperator = (operator) => {
+    this.setState({
+      mathOperator: operator
+    })
+    console.log('in setOperator', operator);
+  }
+
+  runMath = () => {
+    console.log(this.state);
+  }
+
   render() {
     return (
-      <div className="App">
+      <div>
         <form></form>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>/</button>
+        <button onClick={ () => this.firstValue(1)}>1</button>
+        <button onClick={ () => this.firstValue(2)}>2</button>
+        <button onClick={ () => this.firstValue(3)}>3</button>
+        <button onClick={ () => this.setOperator('/')}>/</button>
         <br></br>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>*</button>
+        <button onClick={ () => this.firstValue(4)}>4</button>
+        <button onClick={ () => this.firstValue(5)}>5</button>
+        <button onClick={ () => this.firstValue(6)}>6</button>
+        <button onClick={ () => this.setOperator('*')}>*</button>
         <br></br>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>+</button>
+        <button onClick={ () => this.firstValue(7)}>7</button>
+        <button onClick={ () => this.firstValue(8)}>8</button>
+        <button onClick={ () => this.firstValue(9)}>9</button>
+        <button onClick={ () => this.setOperator('+')}>+</button>
         <br></br>
-        <button>0</button>
-        <button> . </button>
-        <button>=</button>
-        <button>-</button>
+        <button onClick={ () => this.firstValue(0)}>0</button>
+        <button onClick={ () => this.firstValue('.')}> . </button>
+        <button onClick={ () => this.runMath()}>=</button>
+        <button onClick={ () => this.setOperator('-')}>-</button>
       </div>
     );
   }
